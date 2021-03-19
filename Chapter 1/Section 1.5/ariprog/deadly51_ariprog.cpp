c7bcaebc889d167a2d2e5cb6e51918f04d6f3475
Maxim <deadly536@gmail.com>
2021-03-19 19:26:15 +0500

    main commit
    copy some files from another rep

---
+++ Chapter 1/Section 1.5/ariprog/deadly51_ariprog.cpp
@@ -0,0 +1,26 @@
+
+/*
+ID: deadly51
+TASK: ariprog
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <string>
+
+using namespace std;
+int N, M;
+
+
+int main() {
+    ifstream fin("ariprog.in");
+    ofstream fout("ariprog.out");
+    cin >> N >> M;
+    fin.close();
+
+
+    fout.close();
+    return 0;
+}
+
---
+++ Templates/templ.py
@@ -0,0 +1,15 @@
+"""
+ID: 
+TASK: 
+LANG: PYTHON3
+"""
+
+
+# with open(".in", "r") as file:
+#     pass
+
+
+
+# with open(".out", "w") as file:
+#     pass
+
