4cd8c03d86b3c4e0bb9c2c1526467d4d6b3206eb
Maxim <deadly536@gmail.com>
2021-04-16 16:52:00 +0500

    readme v1.0.1

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Hi there! <img src="https://user-images.githubusercontent.com/76053782/113277870-a1440180-92fa-11eb-87b9-32ec5f0c16af.gif" width="40" height="40" />
 
-Here you will find my solution from the university Olympiad programming contests!
+### Here you will find my solution from the university Olympiad programming contests!
+***
+#### This repository consist solutions for:
+
+[Codeforces][codeforces]
+[Usaco Train][usaco]
+
+***
+
+#### My profiles:
+
+Codeforces handle: [maxim_belyakov][codeforcesprof]
+Usaco ID: deadly51
+
+
+[website]: https://google.com/
+[usaco]: https://train.usaco.org/
+[codeforces]: https://codeforces.com/
+[codeforcesprof]: https://codeforces.com/profile/maxim_belyakov
--- usaco/Chapter 1/Section 1.5/ariprog/deadly51_ariprog.cpp
+++ usaco/Chapter 1/Section 1.5/ariprog/deadly51_ariprog.cpp
@@ -1,26 +1,77 @@
-
 /*
 ID: deadly51
 TASK: ariprog
 LANG: C++
 */
+
 #include <iostream>
+#include <algorithm>
 #include <fstream>
-#include <queue>
-#include <string>
+#include <vector>
+#include <set>
+#include <ctime>
 
 using namespace std;
-int N, M;
+
+
+struct res {
+    int first;
+    int step;
+    bool operator<(const res& _entry) const {
+        return step < _entry.step;
+    }
+};
+
 
 
 int main() {
     ifstream fin("ariprog.in");
     ofstream fout("ariprog.out");
-    cin >> N >> M;
+    int n, m;
+    fin >> n >> m;
     fin.close();
+    
+    set<int> collection;
 
+    for (int p = 0; p <= m; p++)
+        for (int q = 0; q <= m; q++)
+            collection.insert(p*p + q*q);
 
+    vector<res> sol;
+    set<int>::iterator ik;
+    unsigned int start_time = clock();
+    for (auto i : collection) {
+        for (ik=--collection.end(); *ik!=1; ik--) {
+            int step = (*ik-i)/(n-1);
+            if(step <= 0 || (*ik-i)%(n-1)!=0) continue;
+            bool flag = true;
+            for (int j=1; j<n; j++) {
+                if (collection.find(i+step*j)==collection.end()) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                res r = {i, step};
+                sol.push_back(r);
+            }
+        }
+    }
+    cout << clock() - start_time << endl;
+    if (sol.size() == 0)
+        fout << "NONE" << endl;
+    else {
+        sort(sol.begin(), sol.end());
+        for (res& r : sol)
+            fout << r.first << " " << r.step << endl;
+    }
     fout.close();
     return 0;
 }
 
+
+// 37 4
+// 5 20
+
+// 0 0
+// 1 0
